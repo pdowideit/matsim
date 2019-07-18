50da2668b9027312642b21928f58e2e2170d73e6
leich <leich@vsp.tu-berlin.de>
2019-07-18 15:44:09 +0200

    bugfix, check first+last activity, better error message (PlanChecker)

--- matsim/src/main/java/org/matsim/core/population/algorithms/PlanChecker.java
+++ matsim/src/main/java/org/matsim/core/population/algorithms/PlanChecker.java
@@ -27,56 +27,89 @@ import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
 
 /**
- * Checks whether if the plan consists only of a sequence of activities and legs,
- * starting and ending with an activity and with no two activities or two legs directly
- * one after another.
+ * Checks whether if the plan consists only of a sequence of activities and
+ * legs, starting and ending with an activity and with no two activities or two
+ * legs directly one after another.
  * 
  * @author gleich
  *
  */
 public class PlanChecker implements PlanAlgorithm {
-	
-	private static final Logger log = Logger.getLogger(PlanChecker.class); 
-	
+
+	private static final Logger log = Logger.getLogger(PlanChecker.class);
+
 	private enum PlanElementType {
 		Activity, Leg
 	}
+
 	private final String description;
-	
+
 	public PlanChecker(final String description) {
 		this.description = description;
 	}
 
 	@Override
 	public void run(Plan plan) {
-		PlanElementType lastPlanElementType = null;
-		for (PlanElement pe: plan.getPlanElements()) {
+		PlanElementType lastPlanElementType;
+		
+		// Check first PlanElement is an Activity
+		if (plan.getPlanElements().get(0) instanceof Activity) {
+			// ok
+			lastPlanElementType = PlanElementType.Activity;
+		} else {
+			log.error(
+					"Invalid plan: Found first PlanElement which is not an Activity!\n"
+							+ createDetailedErrorMessage(plan));
+			throw new RuntimeException("Invalid Plan");
+		}
+		PlanElement pe;
+		// TODO: Is a plan with 0 or 1 plan elements valid?
+		// Check that after a Leg only an Activity can follow and after an Activity only a Leg can follow 
+		for (int i = 1; i < plan.getPlanElements().size(); i++) {
+			pe = plan.getPlanElements().get(i);
 			if (pe instanceof Activity) {
-				if (lastPlanElementType == null || lastPlanElementType.equals(PlanElementType.Leg)) {
+				if (lastPlanElementType.equals(PlanElementType.Leg)) {
 					// ok
 					lastPlanElementType = PlanElementType.Activity;
 				} else {
-					log.error("Invalid plan: Found Activity which is neither the first PlanElement of the plan nor located after a Leg!\n"
-					 + "PlanChecker description: " + description + "\n"
-					 + "Invalid plan: " + plan.toString());
+					log.error(
+							"Invalid plan: Found Activity after a PlanElement which is not a Leg!\n"
+									+ createDetailedErrorMessage(plan));
 					throw new RuntimeException("Invalid Plan");
 				}
 			} else if (pe instanceof Leg) {
-				if (lastPlanElementType.equals(PlanElementType.Leg)) {
+				if (lastPlanElementType.equals(PlanElementType.Activity)) {
 					// ok
 					lastPlanElementType = PlanElementType.Leg;
 				} else {
 					log.error("Invalid plan: Found Leg after a PlanElement which is not an Activity!\n"
-					 + "PlanChecker description: " + description + "\n"
-					 + "Invalid plan: " + plan.toString());
+							+ createDetailedErrorMessage(plan));
 					throw new RuntimeException("Invalid Plan");
 				}
 			} else {
-				log.error("Unknown PlanElement type: " + pe.getClass().getName() + "\n"
-				 + "PlanChecker description: " + description + "\n"
-				 + "Invalid plan: " + plan.toString());
+				log.error("Unknown PlanElement type: " + pe.getClass().getName() + "\n" + createDetailedErrorMessage(plan));
 				throw new RuntimeException("Unknown PlanElement type");
 			}
 		}
+		// Check last PlanElement is an Activity
+		if (!lastPlanElementType.equals(PlanElementType.Activity)) {
+			log.error("Invalid plan: Last PlanElement is not an Activity!\n"
+					+ createDetailedErrorMessage(plan));
+			throw new RuntimeException("Invalid Plan");
+		}
+	}
+
+	private String createDetailedErrorMessage(Plan plan) {
+		StringBuilder strBuild = new StringBuilder();
+		strBuild.append("PlanChecker description: ");
+		strBuild.append(description);
+		strBuild.append("\nInvalid plan: ");
+		strBuild.append(plan.toString());
+		strBuild.append("\nPlanElements: ");
+		for (PlanElement pe : plan.getPlanElements()) {
+			strBuild.append(pe.toString());
+			strBuild.append(", ");
+		}
+		return strBuild.toString();
 	}
 }
